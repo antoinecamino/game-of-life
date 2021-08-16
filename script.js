10cb7e0a7e85d0622060d378d6ea10dadc840255
antoinecamino <82501754+antoinecamino@users.noreply.github.com>
2021-08-16 16:33:57 +0200

    js stable

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-let tab=[];
-let nblignes=30;
-let nbcolonne=30;
-
-
-for (let i = 0; i < nbcolonne; i++) {
-  
-    tab.push;
-    console.log(tab)
+let tab = [];
+let nbLignes = 30;
+let nbCol = 30;
+let blanc = 0;
+let noir = 1 ;
 
 
 
-  }
+for (let i =0; i< nbLignes ; i++){
+    let newTab = [];
+    
+    for (let j=0; j<nbCol; j++){
+        newTab.push(blanc);
+    }
+
+    tab.push(newTab);
+}
+console.table(tab);
