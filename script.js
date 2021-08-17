28e63c94a350b334e6672c8f9b1527b4ef45ec53
antoinecamino <82501754+antoinecamino@users.noreply.github.com>
2021-08-17 13:51:07 +0200

    ajout de la fonctionnalité des cases au click

--- script.js
+++ script.js
@@ -1,40 +1,74 @@
-
-let b = document.body;
-
 let tab = [];
-let nbLignes = 30;
+let nbLignes = 27;
 let nbCol = 30;
 let blanc = 0;
 let noir = 1;
 
+let b = document.body;
+let cellule = document.createElement("div");
 
 
+function draw() {
+    let newTabHtml = document.createElement("table");
+    let newtBodyHtml = document.createElement("tBody");
+    // let newTab = [];
+    for (let i = 0; i < nbLignes; i++) {
+        let row = document.createElement("tr");
 
 
+        for (let j = 0; j < nbCol; j++) {
+             let cell = document.createElement("td");
 
-for (let i = 0; i < nbLignes; i++) {
-    let newTab = [];
+            
+            cell.addEventListener("click", e => {
 
-    for (let j = 0; j < nbCol; j++) {
-        let cellule = document.createElement("div");
-        newTab.push(cellule);
-        b.append(cellule);
+                
 
+                if (cell.className != 'click') {
+                    cell.className = 'click';
+                } else  {
+                    cell.className = "";
+                }
+                console.log(cell.className);
+
+            });
+
+            row.appendChild(cell);
+
+
+        }
+
+        newtBodyHtml.appendChild(row);
+
+        // tab.push(newTab);
     }
+    newTabHtml.appendChild(newtBodyHtml);
+    b.append(newTabHtml);
+    console.table(tab);
 
-    tab.push(newTab);
+
+
+
+
+
 }
-console.table(tab);
+
+
+
+draw();
+
+
+// b.append(tab);
 
 
 
 
 
+ // if (i === 0) {
+    //     cell.style.background = "rgb(255, 250, 250)";
+    // } else {
+    //     cell.style.background = "rgb(0, 0, 0)";
+    // }
 
 
 
-//cellule.textContent= draw(); 
-
-//cellule.style.backgroundColor="blue"
-
-//  elements += "<li>" + texteContact(contacts[i]) + "</li>";
