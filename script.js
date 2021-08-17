d7a6917e6c78246dadff3b324206704b2cafb1d2
antoinecamino <82501754+antoinecamino@users.noreply.github.com>
2021-08-17 10:49:59 +0200

    correction js ajout des div

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
+
+let b = document.body;
+
 let tab = [];
 let nbLignes = 30;
 let nbCol = 30;
 let blanc = 0;
-let noir = 1 ;
+let noir = 1;
 
 
 
-for (let i =0; i< nbLignes ; i++){
+
+
+
+for (let i = 0; i < nbLignes; i++) {
     let newTab = [];
-    
-    for (let j=0; j<nbCol; j++){
-        newTab.push(blanc);
+
+    for (let j = 0; j < nbCol; j++) {
+        let cellule = document.createElement("div");
+        newTab.push(cellule);
+        b.append(cellule);
+
     }
 
     tab.push(newTab);
 }
 console.table(tab);
+
+
+
+
+
+
+
+
+//cellule.textContent= draw(); 
+
+//cellule.style.backgroundColor="blue"
+
+//  elements += "<li>" + texteContact(contacts[i]) + "</li>";
