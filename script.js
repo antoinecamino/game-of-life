e300931b510aa83ad8cdf093884d19700cf46dfd
jerem-greencat <82501643+jerem-greencat@users.noreply.github.com>
2021-08-17 16:18:40 +0200

    début règle survie
    implantation règle survie >2<3

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let nbLignes = 30;
 let nbCol = 30;
 
 let b = document.body;
-let cellule = document.createElement("div"); 
+let cellule = document.createElement("div");
+let blanc = 0;
+let noir =1; 
 
 
 function draw(){   
@@ -18,6 +20,8 @@ function draw(){
         
         for (let j=0; j<nbCol; j++){            
             let cell = document.createElement("td");
+            let numNeighbours = 0;
+
             row.appendChild(cell)
 
             cell.addEventListener("click", function(e){
@@ -29,20 +33,28 @@ function draw(){
             })
 
 
+            if (newTabHtml[i][j].className === "click"){
+                for (let k = i-1; k< i+1; k++ ){
+                    for (let l= j-1; l< i+1; l++){
+                        if (newTabHtml[k][l].className !== "click"){
+                            continue;
+                        }else{
+                            numNeighbours ++;
+                        }
+                    }
+                }
+                if (numNeighbours<2 || numNeighbours>3){
+                    newTabHtml[i][j].classList.remove("click");
+                }
+            }
 
-
-           
 
         }
         newtBodyHtml.appendChild(row);
 
     }
     newTabHtml.appendChild(newtBodyHtml);
-    b.append(newTabHtml);
-
-   
-
-    
+    b.append(newTabHtml); 
 }
 
 draw();
