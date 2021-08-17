beef6eb46d4a6617bc33b6670cdddfe08d86e648
jerem-greencat <82501643+jerem-greencat@users.noreply.github.com>
2021-08-17 12:09:32 +0200

    J'ai des cellules de ma branche qui changent de couleur negro !!!
    Cell passent de vides a pleines !

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-let tab = [];
+
 let nbLignes = 30;
 let nbCol = 30;
-let blanc = 0;
-let noir = 1 ;
 
 let b = document.body;
 let cellule = document.createElement("div"); 
 
 
-function draw(){     
-    let c = 0;   
+function draw(){   
     let newTabHtml = document.createElement("table");
     let newtBodyHtml = document.createElement("tBody");
+    
+
+
     for (let i =0; i< nbLignes ; i++){
         row = document.createElement("tr");
 
         
-        for (let j=0; j<nbCol; j++){
-            cell = document.createElement("td");
-
+        for (let j=0; j<nbCol; j++){            
+            let cell = document.createElement("td");
             row.appendChild(cell)
 
+            cell.addEventListener("click", function(e){
+                cell.className = "click";
+            })
 
-            if (c === 0) {
-                cell.style.background = "rgb(0,191,255)";
-            } else {
-                cell.style.background = "rgb(255,0,0)";
-            }
+
+           
 
         }
         newtBodyHtml.appendChild(row);
@@ -34,7 +33,8 @@ function draw(){
     }
     newTabHtml.appendChild(newtBodyHtml);
     b.append(newTabHtml);
-    console.table(tab);
+
+   
 
     
 }
